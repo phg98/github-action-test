12f76689f35887c8fb75ac023d12276f72bfc80f
Harry Park <phg98@naver.com>
2020-11-01 15:06:48 +0900

    Add pytest

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    print('hello')
+    return 'hello'
+
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 4
+
+def test_main():
+    assert main() == 'hello'
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
